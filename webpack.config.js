dbbc5ff49191c51e5f099a4e376203d259ce4bab
andrewc2020 <andrewc2020@gmail.com>
2020-09-24 17:58:28 +0100

    removed ref to babel plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const {BabelPlugin} = require('@babel/plugin-syntax-dynamic-import');
+
 
 
 
